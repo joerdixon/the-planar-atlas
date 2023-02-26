3625f065b37f827902f1f5603076da8e2b0df89d
joerdixon <joseph35dixon@gmail.com>
2023-02-26 14:38:38 -0800

    major refactor

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ let destination;
 // Visited Planes
 let visitedPlanes;
 
+let courseObjects = [];
+
 // Array of all possible planes
 const planarAtlas = [
     "The Astral Plane",
@@ -149,6 +151,25 @@ function Dice(sides) {
     return Math.floor(Math.random() * sides) + 1;
 }
 
+// Returns a single plane random plane object.
+function generatePlane() {
+    // Generate the timing.
+    const gateTime = determineTiming();
+
+    return {
+        // The name of the plane.
+        name: planarAtlas[Dice(100) - 1],
+        // Is the gate opening, open, or closing.
+        status: gateStatus[Dice(2) - 1],
+        // Where the gate will open in that plane
+        location: gateLocation[Dice(3) - 1],
+        // How long it will take to get to that gate.
+        travelTime: gateTime[0],
+        // How long until the gate closes.
+        gateClose: gateTime[1]
+    }
+}
+
 // Rolls for the travel time and gate close time.
 // The first number in the return array will always be the travel time.
 function determineTiming() {
@@ -169,7 +190,8 @@ function rerollPlane() {
 }
 
 // Creates an HTML element to be added to the itinerary.
-function createPlaneCard({ name, status, location, travelTime, gateClose }) {
+function createPlaneCard({ name, status, location, travelTime, gateClose }, planeCount) {
+    console.log(planeCount)
     // Main Container
     const planeCard = document.createElement("div");
     planeCard.classList.add("planeCard");
@@ -203,11 +225,14 @@ function createPlaneCard({ name, status, location, travelTime, gateClose }) {
     const rerollButton = document.createElement("button");
     rerollButton.innerText = "Reroll";
     rerollButton.classList.add("rerollButton");
-    rerollButton.setAttribute("onclick", "rerollPlane()")
-    // Skip if 
+    rerollButton.setAttribute("onclick", "rerollPlane()");
+
+    // Skip the destination card.
     if (name != `Destination: ${destinationSelect.value}`) {
         planeCard.appendChild(rerollButton);
     }
+
+    // Return the card element.
     return planeCard;
 }
 
@@ -231,9 +256,8 @@ function createOriginCard() {
     planeCard.appendChild(planeTitle);
     planeCard.appendChild(planeYAH);
 
+    // Return the origin card element.
     return (planeCard)
-
-
 }
 
 // This will print a destination card in the case of maximum planes rolled or the Astral Plane.
@@ -252,8 +276,94 @@ function forceDestinationCard() {
     return (createPlaneCard(lastPlane))
 }
 
+// This function returns a number of planes based on their Arcana check.
+function rollPlanes(arcanaRoll) {
+    switch (true) {
+        case (arcanaRoll <= 5):
+            return (Dice(8) + Dice(8));
+        case (arcanaRoll <= 10):
+            return (Dice(6) + Dice(6));
+        case (arcanaRoll <= 15):
+            return Dice(10)
+        case (arcanaRoll > 15):
+            return Dice(8);
+    }
+}
+
+// This function will take an array of objects and render them one at a time, noting their order. 
+function renderChart(planeObjArray) {
+
+    // Reset the chart
+    chart.innerHTML = "";
+
+    // Counts the planes to keep the order.
+    let planeCount = 0;
+
+    // Create a card for the starting plane.
+    chart.appendChild(createOriginCard())
+
+    // Create an element for each plane on the course.
+    for (planes in planeObjArray) {
+        chart.appendChild(createPlaneCard(planeObjArray[planes], planeCount))
+        planeCount += 1
+    }
+
+    // Create a card for the destination plane.
+    chart.appendChild(forceDestinationCard())
+
+}
+
+// This function will return an initial course in an array of objects.
+function determineCourse(numOfPlanes) {
+
+    // Reset the course tracker
+    courseObjects = [];
+
+    // Reset the visited planes.
+    visitedPlanes = [originSelect.value];
+
+    for (let i = 0; i < numOfPlanes; i++) {
+
+        // Create a random plane
+        const nextPlane = generatePlane();
+
+        // If the plane rolled has already been visited
+        if (visitedPlanes.includes(nextPlane.name)) {
+            // Replace the iteration
+            i -= 1;
+            // Skip the iteration
+            continue;
+        }
+
+        // If we see our origin plane, skip that iteration before we print.
+        if (nextPlane.name === originSelect.value) {
+            continue;
+        }
+
+        // If we reach our destination, stop looping.
+        if (nextPlane.name === destinationSelect.value) {
+            return courseObjects;
+        }
+
+        // Add the plane to the course objects.
+        courseObjects.push(nextPlane)
+
+        // Add the plane to list of planes already on course.
+        visitedPlanes.push(nextPlane.name)
+
+        // If we go to The Astral Plane, the next plane is always our destination.
+        if (nextPlane.name === "The Astral Plane" && destinationSelect.value != "The Astral Plane") {
+            // chart.appendChild(forceDestinationCard())
+            return courseObjects;
+        }
+    }
+
+    return courseObjects;
+}
+
 // When start button is clicked.
 function Chart() {
+
     // Check that they entered something.
     if (arcanaCheck.value === "") {
         alert("Please enter your roll");
@@ -271,90 +381,13 @@ function Chart() {
         return;
     }
 
-    // Reset the chart
-    chart.innerHTML = "";
-
-    // Planes already on the course
-    visitedPlanes = [originSelect.value];
-
-
     // The players roll determines the amount of planes that must be rolled.
-    switch (true) {
-        case (roll <= 5):
-            planes = (Dice(8) + Dice(8));
-            break;
-        case (roll <= 10):
-            planes = (Dice(6) + Dice(6));
-            break;
-        case (roll <= 15):
-            planes = Dice(10)
-            break;
-        case (roll > 15):
-            planes = Dice(8);
-            break;
-    }
+    planes = rollPlanes(roll)
 
-    // Create a card for the starting plane.
-    chart.appendChild(createOriginCard())
+    // We will roll that many planes into a course.
+    let course = determineCourse(planes)
 
-    // Counts the planes as they are added
-    const planeCount = 0;
-
-    // Roll the number of planes specified.
-    for (let i = 0; i < planes; i++) {
-
-        const gateTime = determineTiming();
-        // Create a plane
-        const nextPlane = {
-            // The name of the plane.
-            name: planarAtlas[Dice(100) - 1],
-            // Is the gate opening, open, or closing.
-            status: gateStatus[Dice(2) - 1],
-            // Where the gate will open in that plane
-            location: gateLocation[Dice(3) - 1],
-            // How long it will take to get to that gate.
-            travelTime: gateTime[0],
-            // How long until the gate closes.
-            gateClose: gateTime[1]
-        }
-
-        // If the plane rolled has already been visited
-        if (visitedPlanes.includes(nextPlane.name)) {
-            // Replace the iteration
-            i -= 1;
-            // Skip the iteration
-            continue;
-        }
-
-        // Add the plane to list of planes already on course.
-        visitedPlanes.push(nextPlane.name)
-
-        // If we see our origin plane, skip that iteration before we print.
-        if (nextPlane.name === originSelect.value) {
-            continue;
-        }
-
-        
-        // Create and append a plane card to the chart.
-        // Add the planeCount to keep track of the order.
-        chart.appendChild(createPlaneCard(nextPlane, planeCount))
-        
-        // Increment the planeCount if its valid.
-        planeCount += 1;
-        
-        // If we go to The Astral Plane, the next plane is always our destination.
-        if (nextPlane.name === "The Astral Plane" && destinationSelect.value != "The Astral Plane") {
-            chart.appendChild(forceDestinationCard())
-            return;
-        }
-
-        // If we reach our destination, stop looping.
-        if (nextPlane.name === destinationSelect.value) {
-            return;
-        }
-    }
-
-    // Create a destination card if not found within the scheduled planes.
-    chart.appendChild(forceDestinationCard())
+    // Render the course on the dom.
+    renderChart(course)
 
 }
